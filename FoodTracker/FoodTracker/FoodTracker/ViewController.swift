301309c106944e64895f5ebcc7dc880091bc2975
jussivesa <jussi.vesa@gmail.com>
2017-02-28 20:20:12 +0200

    Add image with camera roll support. Fixed layout in main view.

--- FoodTracker/FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/FoodTracker/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //MARK: Properties
     @IBOutlet weak var foodNameLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,7 +38,42 @@ class ViewController: UIViewController, UITextFieldDelegate {
         foodNameLabel.text = textField.text
     }
     
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        //Dissmiss the picker if user cancels
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //The info dictionary may contain multiple represensations of the image, lets use original
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary to contain an image, but was provided the following: \(info)")
+        }
+        
+        //Set view to display the selected image
+        photoImageView.image = selectedImage
+        
+        //Dissmiss picker
+        dismiss(animated: true, completion: nil)
+    }
+    
     //MARK: Actions
+    //Tap gesture recognizer for image
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        //Hide keyboard
+        nameTextField.resignFirstResponder()
+        
+        //ViewController, lets user to select photo from library
+        let imagePickerController = UIImagePickerController()
+        
+        //Only allow photos to be picked, not taken
+        imagePickerController.sourceType = .photoLibrary // .camera etc
+        
+        //Make sure ViewController is notified at img select
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         foodNameLabel.text = "Default label text"
     }
